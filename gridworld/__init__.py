c3f3707ac1a4c3d2478daf524022eae6049858da
giangbang <banggiangle2015@gmail.com>
2023-02-02 15:33:16 +0700

    finish

--- gridworld/__init__.py
+++ gridworld/__init__.py
@@ -6,3 +6,22 @@ register(
     entry_point=__name__ + ":GridworldEnv",
     max_episode_steps=100,
 )
+
+register(
+    id="Gridworld-H1-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":4, "generate_goal":False, "random_start":True}
+)
+register(
+    id="Gridworld-H2-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":5, "generate_goal":False, "random_start":True}
+)
+register(
+    id="Gridworld-H3-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":6, "generate_goal":False, "random_start":True}
+)
