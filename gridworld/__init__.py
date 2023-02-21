6e6a1ed5547f5b8b9efc0926f90f4dde6ce7fed4
giangbang <banggiangle2015@gmail.com>
2023-02-22 01:12:01 +0700

    add several sparse reward envs

--- gridworld/__init__.py
+++ gridworld/__init__.py
@@ -25,3 +25,63 @@ register(
     max_episode_steps=100,
     kwargs={"plan":6, "generate_goal":False, "random_start":True}
 )
+
+# sparse reward environments
+register(
+    id="Gridworld-H1-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":4, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
+register(
+    id="Gridworld-H2-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":5, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
+register(
+    id="Gridworld-H3-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":6, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
+
+# maze environments
+register(
+    id="Gridworld-Maze1-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":30, "generate_goal":False, "random_start":True}
+)
+register(
+    id="Gridworld-Maze2-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":31, "generate_goal":False, "random_start":True}
+)
+register(
+    id="Gridworld-Maze3-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":32, "generate_goal":False, "random_start":True}
+)
+
+# sparse reward maze environments
+register(
+    id="Gridworld-Maze1-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":30, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
+register(
+    id="Gridworld-Maze2-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":31, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
+register(
+    id="Gridworld-Maze3-s-v0",
+    entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+    kwargs={"plan":32, "generate_goal":False, "random_start":True, "sparse_reward":True}
+)
